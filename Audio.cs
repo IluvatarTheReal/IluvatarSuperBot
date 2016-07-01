2dd5e9b91b02a6cfb71cb2d024eb0cb6bc4a885f
Samuel <reidsamuel@hotmail.com>
2016-07-01 19:43:59 -0400

    Music Time
    Added music, setting are hardcoded for now, and still need to be adapted
    for user command

---
+++ Audio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using Discord.Commands.Permissions.Levels;
+
+namespace DiscordBot
+{
+    public static class Audio
+    {
+        public static void StartMusic(string filePath, DiscordClient discordBot, Channel voiceChannel, IAudioClient aService)
+        {
+            var channelCount = discordBot.GetService<AudioService>().Config.Channels; // Get the number of AudioChannels our AudioService has been configured to use.
+            var OutFormat = new WaveFormat(48000, 16, channelCount); // Create a new Output Format, using the spec that Discord will accept, and with the number of channels that our client supports.
+            using (var AudioReader = new AudioFileReader(filePath)) // Create a new Disposable MP3FileReader, to read audio from the filePath parameter
+            using (var resampler = new MediaFoundationResampler(AudioReader, OutFormat)) // Create a Disposable Resampler, which will convert the read MP3 data to PCM, using our Output Format
+            {
+                resampler.ResamplerQuality = 60; // Set the quality of the resampler to 60, the highest quality
+                int blockSize = OutFormat.AverageBytesPerSecond / 50; // Establish the size of our AudioBuffer
+                byte[] buffer = new byte[blockSize];
+                int byteCount;
+
+                while ((byteCount = resampler.Read(buffer, 0, blockSize)) > 0) // Read audio into our buffer, and keep a loop open while data is present
+                {
+                    if (byteCount < blockSize)
+                    {
+                        // Incomplete Frame
+                        for (int i = byteCount; i < blockSize; i++)
+                            buffer[i] = 0;
+                    }
+                    aService.Send(buffer, 0, blockSize); // Send the buffer to Discord
+                }
+            }
+        }
+    }
+}
